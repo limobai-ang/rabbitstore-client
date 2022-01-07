f17b3c6dbb22711d17cbe8fd8e4435e388f5283e
liang <2501119005@qq.com>
2022-01-07 21:28:08 +0800

    初始化项目目录

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userName: '张三'
   },
   mutations: {
+    // 可以定义修改state数据的方法接收两个参数state和step 通过commit触发事件
   },
   actions: {
+    // 可以处理异步的方法接收两个参数context和传递的参数step 通过dispatch属性触发
+  },
+  getters: {
+    // vuex中的计算属性
+    getName (state) {
+      return state.userName + '!!!'
+    }
   },
   modules: {
+    // 模块化数据
   }
 })
---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    hello
+  </div>
+  <p>userName: {{$store.state.userName}}</p>
+  <p>vuex计算属性: {{$store.getters.getName}}</p>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'Home',
+  setup () {
+    // 使用vuex仓库
+    const store = useStore()
+    console.log(store.state.userName)
+    console.log(store.getters.getName)
+  }
+}
+</script>
