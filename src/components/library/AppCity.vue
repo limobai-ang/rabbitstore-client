2e405310e58b9f577a6e4b4746f6bef981001a10
liang <2501119005@qq.com>
2022-01-22 22:35:05 +0800

    完成了选择地区功能组件的封装

--- src/components/library/AppCity.vue
+++ src/components/library/AppCity.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="app-city" ref="target">
-    <div class="select" @click="active = true" :class="{active}">
-      <span class="placeholder">请选择配送地址</span>
+    <div class="select" @click="open" :class="{ active }">
+      <span class="placeholder">{{ changeResult.fullLocation || fullLocation }}</span>
       <span class="value"></span>
       <i class="iconfont icon-angle-down"></i>
     </div>
+    <!-- 省市区选项区域  active控制显示或者隐藏-->
     <div class="option" v-if="active">
-      <span class="ellipsis" v-for="i in 24" :key="i">北京市</span>
+      <div v-if="loading" class="loading"></div>
+      <template v-else>
+        <span class="ellipsis" v-for="item in currList" :key="item.code" @click="changeItem(item)">{{item.name}}</span>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue-demi'
+import { ref, reactive, computed } from 'vue-demi'
 import { onClickOutside } from '@vueuse/core'
+import axios from 'axios'
 export default {
   name: 'AppCity',
-  setup () {
+  props: {
+    fullLocation: {
+      type: String,
+      default: '请选择配送地址'
+    }
+  },
+  setup (props, { emit }) {
     // 控制展开收起,默认收起
     const active = ref(false)
+
+    // 展开收起方法
     const openDialog = () => {
       active.value = true
     }
@@ -35,59 +48,151 @@ export default {
     onClickOutside(target, () => {
       closeDialog()
     })
-    return { active, toggleDialog, target }
+
+    // 获取城市数据
+    // 2. 何时获取？打开城市列表的时候，做个内存中缓存
+    const getCityData = () => {
+      // 这个位置可能有异常操作，封装成promise
+      return new Promise((resolve, reject) => {
+        if (window.cityData) {
+          // 有缓存
+          resolve(window.cityData)
+        } else {
+          // 无缓存
+          const url = 'https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json'
+          axios.get(url).then((res) => {
+            window.cityData = res.data
+            resolve(window.cityData)
+          })
+        }
+      })
+    }
+
+    // 获取城市数据,  获取数据时开启loading效果
+    const loading = ref(false)
+    const cityData = ref([])
+    loading.value = true
+    // 获取数据
+    getCityData().then((data) => {
+      cityData.value = data
+      loading.value = false
+    })
+
+    // 点击开启选择地址方法
+    const open = () => {
+      active.value = true
+      // 清空选择结果
+      for (const key in changeResult) {
+        changeResult[key] = ''
+      }
+    }
+
+    // 地址信息
+    const changeResult = reactive({
+      provinceCode: '',
+      provinceName: '',
+      cityCode: '',
+      cityName: '',
+      countyCode: '',
+      countyName: '',
+      fullLocation: ''
+    })
+    // 收集获取地址信息
+    const changeItem = (item) => {
+      // 省份
+      if (item.level === 0) {
+        changeResult.provinceCode = item.code
+        changeResult.provinceName = item.name
+        changeResult.fullLocation = `${changeResult.provinceName} 请选择 请选择`
+      }
+      // 市
+      if (item.level === 1) {
+        changeResult.cityCode = item.code
+        changeResult.cityName = item.name
+        changeResult.fullLocation = `${changeResult.provinceName} ${changeResult.cityName} 请选择`
+      }
+      // 地区
+      if (item.level === 2) {
+        changeResult.countyCode = item.code
+        changeResult.countyName = item.name
+        changeResult.fullLocation = `${changeResult.provinceName} ${changeResult.cityName} ${changeResult.countyName}`
+        // 将选择后的数据发送给父组件，让后关闭选择框
+        emit('change', changeResult)
+        closeDialog()
+      }
+    }
+    // 定义计算属性 获取地址列表 ，根据点击的省份城市展示
+    const currList = computed(() => {
+      // 省份
+      let currList = cityData.value
+      // 城市
+      if (changeResult.provinceCode) {
+        currList = currList.find(p => p.code === changeResult.provinceCode).areaList
+      }
+      // 地区
+      if (changeResult.cityCode) {
+        currList = currList.find(c => c.code === changeResult.cityCode).areaList
+      }
+      return currList
+    })
+    return { active, toggleDialog, target, open, loading, changeResult, changeItem, currList }
   }
 }
 </script>
 
 <style scoped lang="less">
-.app-city {
-  display: inline-block;
-  position: relative;
-  z-index: 400;
-  .select {
-    border: 1px solid #e4e4e4;
-    height: 30px;
-    padding: 0 5px;
-    line-height: 28px;
-    cursor: pointer;
-    &.active {
-      background: #fff;
-    }
-    .placeholder {
-      color: #999;
-    }
-    .value {
-      color: #666;
-      font-size: 12px;
-    }
-    i {
-      font-size: 12px;
-      margin-left: 5px;
-    }
-  }
-  .option {
-    width: 542px;
-    border: 1px solid #e4e4e4;
-    position: absolute;
-    left: 0;
-    top: 29px;
-    background: #fff;
-    min-height: 30px;
-    line-height: 30px;
-    display: flex;
-    flex-wrap: wrap;
-    padding: 10px;
-    > span {
-      width: 130px;
-      text-align: center;
+  .app-city {
+    display: inline-block;
+    position: relative;
+    z-index: 400;
+    .select {
+      border: 1px solid #e4e4e4;
+      height: 30px;
+      padding: 0 5px;
+      line-height: 28px;
       cursor: pointer;
-      border-radius: 4px;
-      padding: 0 3px;
-      &:hover {
-        background: #f5f5f5;
+      &.active {
+        background: #fff;
+      }
+      .placeholder {
+        color: #999;
+      }
+      .value {
+        color: #666;
+        font-size: 12px;
+      }
+      i {
+        font-size: 12px;
+        margin-left: 5px;
+      }
+    }
+    .option {
+      width: 542px;
+      border: 1px solid #e4e4e4;
+      position: absolute;
+      left: 0;
+      top: 29px;
+      background: #fff;
+      min-height: 30px;
+      line-height: 30px;
+      display: flex;
+      flex-wrap: wrap;
+      padding: 10px;
+      .loading {
+        height: 290px;
+        width: 100%;
+        background: url(../../assets/images/load.gif) no-repeat center;
+      }
+      > span {
+        width: 130px;
+        text-align: center;
+        cursor: pointer;
+        border-radius: 4px;
+        padding: 0 3px;
+        &:hover {
+          background: #f5f5f5;
+        }
       }
     }
   }
-}
 </style>
--- src/views/goods/components/GoodsInfoSpec.vue
+++ src/views/goods/components/GoodsInfoSpec.vue
@@ -1,9 +1,9 @@
 <template>
-  <p class="g-name">{{goods.name}}</p>
-  <p class="g-desc">{{goods.desc}}</p>
+  <p class="g-name">{{ goods.name }}</p>
+  <p class="g-desc">{{ goods.desc }}</p>
   <p class="g-price">
-    <span>{{goods.price}}</span>
-    <span>{{goods.oldPrice}}</span>
+    <span>{{ goods.price }}</span>
+    <span>{{ goods.oldPrice }}</span>
   </p>
   <div class="g-service">
     <dl>
@@ -12,8 +12,8 @@
     </dl>
     <dl>
       <dt>配送</dt>
-      <dd style="padding-right: 5px;">至 </dd>
-      <AppCity />
+      <dd style="padding-right: 5px">至</dd>
+      <AppCity :fullLocation="fullLocation" @change="changeCity" />
     </dl>
     <dl>
       <dt>服务</dt>
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: 'GoodsInfoSpec',
   props: {
@@ -35,66 +36,93 @@ export default {
       type: Object,
       default: () => {}
     }
+  },
+  setup (props) {
+    // 默认情况
+    // 提供给后台的4个数据
+    const provinceCode = ref('110000')
+    const cityCode = ref('119900')
+    const countyCode = ref('110101')
+    const fullLocation = ref('北京市 市辖区 东城区')
+
+    // 选择城市 将获取的数据赋值
+    const changeCity = (result) => {
+      provinceCode.value = result.provinceCode
+      cityCode.value = result.cityCode
+      countyCode.value = result.countyCode
+      fullLocation.value = result.fullLocation
+    }
+
+    // 有默认地址, 登录状态后台会返回信息会有位置信息
+    if (props.goods.userAddresses) {
+      const defaultAddr = props.goods.userAddresses.find(
+        (addr) => addr.isDefault === 1
+      )
+      if (defaultAddr) {
+        changeCity(defaultAddr)
+      }
+    }
+    return { fullLocation, open, changeCity }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.g-name {
-  font-size: 22px
-}
-.g-desc {
-  color: #999;
-  margin-top: 10px;
-}
-.g-price {
-  margin-top: 10px;
-  span {
-    &::before {
-      content: "¥";
-      font-size: 14px;
-    }
-    &:first-child {
-      color: @priceColor;
-      margin-right: 10px;
-      font-size: 22px;
-    }
-    &:last-child {
-      color: #999;
-      text-decoration: line-through;
-      font-size: 16px;
+  .g-name {
+    font-size: 22px;
+  }
+  .g-desc {
+    color: #999;
+    margin-top: 10px;
+  }
+  .g-price {
+    margin-top: 10px;
+    span {
+      &::before {
+        content: "¥";
+        font-size: 14px;
+      }
+      &:first-child {
+        color: @priceColor;
+        margin-right: 10px;
+        font-size: 22px;
+      }
+      &:last-child {
+        color: #999;
+        text-decoration: line-through;
+        font-size: 16px;
+      }
     }
   }
-}
-.g-service {
-  background: #f5f5f5;
-  width: 500px;
-  padding: 20px 10px 0 10px;
-  margin-top: 10px;
-  dl {
-    padding-bottom: 20px;
-    display: flex;
-    align-items: center;
-    dt {
-      width: 50px;
-      color: #999;
-    }
-    dd {
-      color: #666;
-      &:last-child {
-        span {
-          margin-right: 10px;
-          &::before {
-            content: "•";
-            color: @xtxColor;
-            margin-right: 2px;
+  .g-service {
+    background: #f5f5f5;
+    width: 500px;
+    padding: 20px 10px 0 10px;
+    margin-top: 10px;
+    dl {
+      padding-bottom: 20px;
+      display: flex;
+      align-items: center;
+      dt {
+        width: 50px;
+        color: #999;
+      }
+      dd {
+        color: #666;
+        &:last-child {
+          span {
+            margin-right: 10px;
+            &::before {
+              content: "•";
+              color: @xtxColor;
+              margin-right: 2px;
+            }
           }
-        }
-        a {
-          color: @xtxColor;
+          a {
+            color: @xtxColor;
+          }
         }
       }
     }
   }
-}
 </style>
